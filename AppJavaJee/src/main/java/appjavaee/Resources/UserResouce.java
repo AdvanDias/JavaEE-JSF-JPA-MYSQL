171c4bdd7dac7069e5f09197a640b95a998c665c
AdvanDias <advan_dias@hotmail.com>
2021-05-08 23:02:02 -0300

    implementando método recebeDados na classe UserResouce para receber os dados da pagina Cadastro

--- AppJavaJee/src/main/java/appjavaee/Resources/UserResouce.java
+++ AppJavaJee/src/main/java/appjavaee/Resources/UserResouce.java
@@ -2,9 +2,46 @@ package appjavaee.Resources;
 
 import java.io.Serializable;
 
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import appjavaee.Entidades.Telefone;
+import appjavaee.Entidades.User;
+
+@Named
+@SessionScoped
 public class UserResouce implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 
+	@Inject
+	private User user;
+	
+	@Inject
+	private Telefone tel;
+	
+	public String RecebeDados() {
+		User u = new User(null, user.getNome(), user.getEmail(), user.getSenha());
+		Telefone phone = new Telefone(null, tel.getDdd(), tel.getNumero(), tel.getTipo(), u);
+		return "";
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Telefone getTel() {
+		return tel;
+	}
+
+	public void setTel(Telefone tel) {
+		this.tel = tel;
+	}
+	
 	
 }
